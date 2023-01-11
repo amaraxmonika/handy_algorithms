d182ac58b930661e63ae38d1f37f6f3178a692d6
Chasen Johnson <chasenjohnson@email.arizona.edu>
2023-01-10 19:53:19 -0700

    adds clever solution to shifted array

--- array_shift.cpp
+++ array_shift.cpp
@@ -29,6 +29,7 @@ void shiftArrayOnce(vector<int>& nums) {
 // Shifts every element in the array to the right by n times. The elemets at
 // the back will wrap around and be written to the front.
 // Ex. {1, 2, 3, 4, 5}, shift 3 => {3, 4, 5, 1, 2}
+// This solution is broken because I exceed the bounds for the input array.
 void shiftArrayN(vector<int>& nums, int count) {
 
     // set to number of rotations and prefill with 0
@@ -42,6 +43,8 @@ void shiftArrayN(vector<int>& nums, int count) {
 
     for (int i = 0; i < nums.size(); i+=count) {
         // int current = nums[i];
+        printf("vars: i=%d, nums.size()=%d\n", i, nums.size());
+
         for (int j = 0; j < current.size(); j++) {
             current[j] = nums[i+j];
         }
@@ -54,6 +57,21 @@ void shiftArrayN(vector<int>& nums, int count) {
         }
         // prev = current;
     }
+}
+
+// This solution works because it brings all the wrapped elements to the front
+// by reversing the entire array.
+// So first we have count=3 and the array
+// {a, b, c, d, e, f} which is then reversed so we have
+// {f, e, d, c, b, a} Now the clever part is where the shifted values are
+//                    un-reversed in their own sub-array.
+// {d, e, f, c, b, a} Finally we un-reverse the second sub-array yielding
+// {d, e, f, a, b, c}.
+void fancyShiftArrayN(vector<int>& nums, int count) {
+    int shiftLen = count % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end() + shiftLen);
+    reverse(nums.begin() + shiftLen, nums.end());
 }
 
 
