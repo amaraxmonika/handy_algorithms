342efd5146599cd4ca5192e193e8b8f734164d54
Chasen Johnson <chasenjohnson@email.arizona.edu>
2023-01-30 13:22:25 -0700

    fixing quicksort

--- arrays/quicksort.cpp
+++ arrays/quicksort.cpp
@@ -1,36 +1,53 @@
 
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
+// How to shift elements in an array
+void printArray(vector<int>& nums) {
+    for (auto &i : nums) {
+        cout << i << ",";
+    }
+    cout << endl;
+}
+
+
+int partition(vector<int>& nums, int left, int right) {
+    int pivot = nums[left + ((right - left) / 2)]; 
+    while (left <= right) {
+        while (nums[left] < pivot) {
+            left++;
+        }
+        while (nums[right] > pivot) {
+            right--;
+        }
+
+        if (left <= right) {
+            int tmp = nums[right];
+            nums[right] = nums[left];
+            nums[left] = tmp;
+            left++;
+            right--;
+        }
+    }
+    return left;
+}
 void quickSort(vector<int>& nums, int left, int right) {
-    int i = left;
-    int j = right;
-    int tmp;
-    int pivot = nums[(left + right) / 2];
+    int index = partition(nums, left, right);
+    if (left < index - 1) {
+        // sort left half
+        quickSort(nums, left, index - 1);
+    }
+    if (index < right) {
+        // sort right half
+        quickSort(nums, index, right);
+    }
+}
 
-    // partition step
-    while (i <= j) {
-        while (nums[i] < pivot) {
-            i++;
-        }
-        while (nums[j] > pivot) {
-            j--;
-        }
-        if (j <= i) {
-            tmp = nums[j];
-            nums[i] = nums[j];
-            nums[j] = tmp;
-            i++;
-            j--;
-        }
-    }
-
-    // recursive step
-    if (left < j) {
-        quickSort(nums, left, j);
-    }
-    if (i < right) {
-        quickSort(nums, i, right);
-    }
+int main() {
+    vector<int> nums {9, 8, 7, 6, 5, 4, 3, 1, 2};
+    printArray(nums);
+    quickSort(nums, 0, nums.size() - 1);
+    printArray(nums);
 }
